4909610316557a6d829da3827eba3f7908dcc8ad
Pashkova <malkusha@mail.ru>
2023-09-27 02:00:23 +0300

    3000

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const { errorHandler } = require('./errors/index');
 
-const { PORT = 3002, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 app.use(cors({
